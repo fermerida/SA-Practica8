b40c3f6df60a6a4de833565934d8b09d75a9d5e4
Fernando Merida <fer.merida94@gmail.com>
2020-10-03 22:09:40 -0600

    Added containers

---
+++ db/sql-scripts/fill.sql
@@ -0,0 +1,8 @@
+INSERT INTO product (name, description,price)
+VALUES
+    ('Control', 'control para PS4', '65'),
+    ('Consola', 'consola PS4', '200'),
+    ('E-watch', 'reloj para dispositivos android', '150'),
+    ('Computadora Dell', '4GB memoria ram, 200 GB de disco duro', '440'),
+    ('Computadora HP', '4GB memoria ram, 200 GB de disco duro', '400'),
+    ('Disco duro', 'disco de estado solido', '230');
